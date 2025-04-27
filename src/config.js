c8340a6112b47ebea8cbb9873dd1be8ebe7d6897
Anthony Mason <Anthony81799@gmail.com>
2025-04-27 15:54:22 -0400

    Deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://anthony81799-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://anthony81799-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+export const API_URL = "<add your server side url>"
 console.log(
     "API_URL :",
     API_URL
